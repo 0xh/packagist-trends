7e341336880ad1f2c126df7d13030cb6a6d9173c
mosaxiv <mosaxiv@gmail.com>
2018-02-17 16:36:29 +0900

    add react-router

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Main from './Main';
 import Header from './Header';
 import Footer from './Footer';
 import Reboot from 'material-ui/Reboot';
+import { Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          minHeight: '100vh',
-          flexDirection: 'column',
-        }}
-      >
-        <Reboot />
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    );
-  }
-}
+const style = {
+  display: 'flex',
+  minHeight: '100vh',
+  flexDirection: 'column',
+};
+
+const App = () => (
+  <div style={style}>
+    <Reboot />
+    <Header />
+    <Route path="/" component={Main} />
+    <Footer />
+  </div>
+);
 
 export default App;
--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
+import queryString from 'query-string';
 import moment from 'moment';
 import Chart from './Carte';
 import Search from './Search';
@@ -17,13 +18,31 @@ class Main extends Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.setGithubStats = this.setGithubStats.bind(this);
+    this.setPackageStats = this.setPackageStats.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    const {q} = queryString.parse(nextProps.location.search, {arrayFormat: 'bracket'});
+
+    for(let v of q) {
+      this.setGithubStats(v);
+      this.setPackageStats(v);
+    }
+  }
   handleSubmit(value) {
     if (this.state.words.includes(value)) {
       return;
     }
 
+    const {location, history} = this.props;
+    const q = queryString.parse(location.search, {arrayFormat: 'bracket'}).q || [];
+    const query = queryString.stringify({q: [...q, value]}, {arrayFormat: 'bracket'});
+    history.push(`?${query}`);
+  }
+
+
+  setGithubStats(value) {
     axios.get(`https://packagist.org/packages/${value}.json`).then(response => {
       const lib = response.data.package;
       this.setState({
@@ -40,17 +59,18 @@ class Main extends Component {
         ],
       });
     });
+  }
 
-    axios
-      .get(`https://packagist.org/packages/${value}/stats/all.json`, {
-        params: {
-          average: 'daily',
-          from: moment()
-            .subtract(6, 'months')
-            .format('YYYY-MM-DD'),
-          to: moment().format('YYYY-MM-DD'),
-        },
-      })
+  setPackageStats(value) {
+    axios.get(`https://packagist.org/packages/${value}/stats/all.json`, {
+      params: {
+        average: 'daily',
+        from: moment()
+          .subtract(6, 'months')
+          .format('YYYY-MM-DD'),
+        to: moment().format('YYYY-MM-DD'),
+      },
+    })
       .then(response => {
         const responseData = response.data;
         let data = this.state.data;
@@ -68,14 +88,14 @@ class Main extends Component {
               },
             ];
           }
-          this.setState({ data: data });
+          this.setState({data: data});
           return;
         }
 
         for (let i in data) {
-          Object.assign(data[i], { [value]: responseData.values[i] });
+          Object.assign(data[i], {[value]: responseData.values[i]});
         }
-        this.setState({ data: data });
+        this.setState({data: data});
       })
       .catch(error => {
         console.error(error);
@@ -106,18 +126,18 @@ class Main extends Component {
   }
 
   render() {
+    const style = {
+      padding: 20,
+      paddingTop: 5,
+      flex: 1,
+    };
+
     return (
-      <main
-        style={{
-          padding: 20,
-          paddingTop: 5,
-          flex: 1,
-        }}
-      >
-        <Search handleSubmit={this.handleSubmit} />
-        <Tag words={this.state.words} handleDelete={this.handleDelete} />
-        <Chart data={this.state.data} />
-        <Github githubStatus={this.state.githubStatus} />
+      <main style={style}>
+        <Search handleSubmit={this.handleSubmit}/>
+        <Tag words={this.state.words} handleDelete={this.handleDelete}/>
+        <Chart data={this.state.data}/>
+        <Github githubStatus={this.state.githubStatus}/>
       </main>
     );
   }
