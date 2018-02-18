b77068c6698403d1be473b99bdf72cdc34f1973d
mosaxiv <mosaxiv@gmail.com>
2018-02-18 14:52:51 +0900

    add icon

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -3,17 +3,24 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import './App.css';
+import logo from '../logo.png';
 
 class Header extends Component {
   render() {
+    let logos = [];
+    for (let i = 0; i < Math.floor(Math.random() * 3) + 1; i++) {
+      logos.push(<img src={logo} key={i} className="App-logo" alt="logo" />);
+    }
+
     return (
       <AppBar position="static" color="default">
         <Toolbar>
           <Typography variant="headline" color="inherit">
-            <a href="/" className="TitleLink">
+            <a href="/" className="Title-link">
               Packagist trends
             </a>
           </Typography>
+          {logos}
         </Toolbar>
       </AppBar>
     );
