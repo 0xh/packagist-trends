d091b33c180f202ef0ee53ca2fbf364d542cc089
mosaxiv <mosaxiv@gmail.com>
2018-02-20 00:39:42 +0900

    add github buttons

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Typography from 'material-ui/Typography';
 import './App.css';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="Footer">
-        <Typography align="center">
-          Made by <a href="https://github.com/mosaxiv">mosaxiv</a>.
-          Thanks for <a href="https://packagist.org/">Packagist</a>.
-        </Typography>
-      </footer>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      <div className="GithubButton">
+        <a
+          className="github-button"
+          href="https://github.com/mosaxiv/packagist-trends"
+          data-icon="octicon-star"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star mosaxiv/packagist-trends on GitHub"
+        >
+          Star
+        </a>
+        <a
+          className="github-button"
+          href="https://github.com/mosaxiv/packagist-trends/issues"
+          data-icon="octicon-issue-opened"
+          data-size="large"
+          aria-label="Issue mosaxiv/packagist-trends on GitHub"
+        >
+          Issue
+        </a>
+      </div>
+      <Typography align="center">
+        Made by <a href="https://github.com/mosaxiv">mosaxiv</a>. Thanks for{' '}
+        <a href="https://packagist.org/">Packagist</a>.
+      </Typography>
+    </footer>
+  );
+};
 
 export default Footer;
--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import './App.css';
 import logo from '../logo.png';
 
-class Header extends Component {
-  render() {
-    let logos = [];
-    for (let i = 0; i < Math.floor(Math.random() * 3) + 1; i++) {
-      logos.push(<img src={logo} key={i} className="App-logo" alt="logo" />);
-    }
-
-    return (
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="headline" color="inherit">
-            <a href="/" className="Title-link">
-              Packagist trends
-            </a>
-          </Typography>
-          {logos}
-        </Toolbar>
-      </AppBar>
-    );
+const Header = () => {
+  let logos = [];
+  for (let i = 0; i < Math.floor(Math.random() * 3) + 1; i++) {
+    logos.push(<img src={logo} key={i} className="App-logo" alt="logo" />);
   }
-}
+
+  return (
+    <AppBar position="static" color="default">
+      <Toolbar>
+        <Typography variant="headline" color="inherit">
+          <a href="/" className="Title-link">
+            Packagist trends
+          </a>
+        </Typography>
+        {logos}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
