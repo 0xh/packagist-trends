d82d2b235a67ae48c8698d3afdcbd8143c245032
mosaxiv <mosaxiv@gmail.com>
2018-05-06 03:00:58 +0900

    Fix Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,30 +5,8 @@ import './App.css';
 const Footer = () => {
   return (
     <footer className="Footer">
-      <div className="GithubButton">
-        <a
-          className="github-button"
-          href="https://github.com/mosaxiv/packagist-trends"
-          data-icon="octicon-star"
-          data-size="large"
-          data-show-count="true"
-          aria-label="Star mosaxiv/packagist-trends on GitHub"
-        >
-          Star
-        </a>
-        <a
-          className="github-button"
-          href="https://github.com/mosaxiv/packagist-trends/issues"
-          data-icon="octicon-issue-opened"
-          data-size="large"
-          aria-label="Issue mosaxiv/packagist-trends on GitHub"
-        >
-          Issue
-        </a>
-      </div>
       <Typography align="center">
-        Made by <a href="https://github.com/mosaxiv">mosaxiv</a>. Thanks for{' '}
-        <a href="https://packagist.org/">Packagist</a>.
+        Thanks for <a href="https://packagist.org/">Packagist</a>.
       </Typography>
     </footer>
   );
